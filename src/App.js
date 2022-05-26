43f5bc87088a96c8b3eaa2ff084916ca51d98641
KashnykovAnton <kashnikov_anton@yahoo.com>
2022-05-26 14:41:54 +0200

    base structure done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from 'react';
+import TodoList from 'components/TodoList';
+import initialTodos from './todos.json';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    todos: initialTodos,
+  };
+
+  deleteTodo = todoId => {
+    this.setState(prevState => ({
+      todos: prevState.todos.filter(todo => todo.id !== todoId),
+    }));
+  };
+
+  render() {
+    const { todos } = this.state;
+    const { deleteTodo } = this;
+
+    const totalTodos = todos.length;
+    const completedTodos = todos.reduce(
+      (acc, todo) => (todo.completed ? acc + 1 : acc),
+      0,
+    );
+    const unCompletedTodos = todos.filter(todo => !todo.completed).length;
+
+    return (
+      <>
+        <h2>My Todo List</h2>
+        <div>
+          <p>Total todos: {totalTodos}</p>
+          <p>Done todos: {completedTodos}</p>
+          <p>Not done todos: {unCompletedTodos}</p>
+        </div>
+        <TodoList todos={todos} onDeleteTodo={deleteTodo} />
+      </>
+    );
+  }
 }
 
 export default App;
