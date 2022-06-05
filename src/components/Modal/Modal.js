fc3de31ab8c6c714520d8647b409da3c72416d6d
KashnykovAnton <kashnikov_anton@yahoo.com>
2022-06-05 18:09:09 +0200

    add modal close btn

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,5 +1,7 @@
 import { Component } from 'react';
 import { createPortal } from 'react-dom';
+import { ReactComponent as CloseIcon } from '../../icons/close.svg';
+import IconButton from 'components/IconButton';
 import './Modal.css';
 
 const modalRoot = document.querySelector('#modal-root');
@@ -34,6 +36,17 @@ class Modal extends Component {
   render() {
     return createPortal(
       <div className="Modal__backdrop" onClick={this.handleBackdropClick}>
+        <div className="Modal__closeBtn">
+          <IconButton
+            type="button"
+            onClick={() => {
+              this.props.onClose();
+            }}
+            aria-label="Close modal"
+          >
+            <CloseIcon width="40px" height="40px" fill="#fff" />
+          </IconButton>
+        </div>
         <div className="Modal__content">{this.props.children}</div>
       </div>,
       modalRoot,
--- src/components/TodoEditor/TodoEditor.js
+++ src/components/TodoEditor/TodoEditor.js
@@ -1,9 +1,24 @@
 import { Component } from 'react';
+
 import './TodoEditor.scss';
 
 class TodoEditor extends Component {
   state = {
     message: '',
+  };
+
+  componentDidMount() {
+    window.addEventListener('keydown', this.handleKeyDown);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handleKeyDown);
+  }
+
+  handleKeyDown = e => {
+    if (e.code === 'Enter' || e.code === 'NumpadEnter') {
+      this.handleSubmit(e);
+    }
   };
 
   handleChange = e => {
@@ -12,6 +27,9 @@ class TodoEditor extends Component {
 
   handleSubmit = e => {
     e.preventDefault();
+    if (!this.state.message.trim()) {
+      return alert("You don't add any information!");
+    }
     this.props.onSubmit(this.state.message);
     this.reset();
   };
@@ -27,6 +45,7 @@ class TodoEditor extends Component {
           className="TodoEditor__textarea"
           value={this.state.message}
           onChange={this.handleChange}
+          placeholder="Add your todo here!"
         ></textarea>
         <button type="submit" className="TodoEditor__button">
           Add Todo
